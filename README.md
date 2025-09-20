a6985c755a86dad44dc824c854e65b1cd51b7ec1
Aaron Ash <aaron.ash@gmail.com>
2025-09-21 07:55:43 +1000

    fix: add checked sum

--- README.md
+++ README.md
@@ -5,19 +5,35 @@ Macbook Pro M3 Max 64GB.
 Rust:
 
 ```
-aaron@MacBook-Pro rust-checked-maths % cargo run --release                                                 
+aaron@MacBook-Pro rust-checked-maths % cargo run --release
    Compiling rust-checked-maths v0.1.0 (/Users/aaron/codebases/rust-checked-maths)
-    Finished `release` profile [optimized] target(s) in 0.36s
+    Finished `release` profile [optimized] target(s) in 0.38s
      Running `target/release/rust-checked-maths`
-rand ints generated in: 6.17595s
-sum calculated in: 202.57ms
-sum: 4476143
+-----------------------------------
+Unchecked maths performance test
+-----------------------------------
+i32
+rand ints generated in: 6.461324s
+sum calculated in: 557.042ms
+sum: 1304940430
 -----------------------------------
 i64
-rand ints generated in: 9.34796s
-sum calculated in: 421.048ms
-sum: 500005883205017
-aaron@MacBook-Pro rust-checked-maths % 
+rand ints generated in: 9.256838s
+sum calculated in: 1.154704s
+sum: 499994696406534
+
+
+-----------------------------------
+CHECKED maths performance test
+-----------------------------------
+i32
+overflow occurred
+sum calculated in: 1.16375s
+sum: Some(499999922704270)
+-----------------------------------
+i64
+sum calculated in: 886.603ms
+sum: Some(499994696406534)
 ```
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,53 @@
+use checked_sum::CheckedSum;
 use rand::Rng;
 use std::time::SystemTime;
 
 fn main() {
+    println!("-----------------------------------");
+    println!("Unchecked maths performance test");
+    println!("-----------------------------------");
+    println!("i32");
     let now = SystemTime::now();
-    let arr: Vec<i32> = (1..=1_000_000_000)
+    let arr_i32: Vec<i32> = (1..=1_000_000_000)
         .map(|_| rand::rng().random_range(1..=1000000))
         .collect();
     println!("rand ints generated in: {:?}", now.elapsed().unwrap());
     let now = SystemTime::now();
-    let s = arr.into_iter().sum::<i32>();
+    let s = arr_i32.clone().into_iter().sum::<i32>();
     println!("sum calculated in: {:?}", now.elapsed().unwrap());
     println!("sum: {:?}", s);
 
     println!("-----------------------------------");
     println!("i64");
     let now = SystemTime::now();
-    let arr: Vec<i64> = (1..=1_000_000_000)
+    let arr_i64: Vec<i64> = (1..=1_000_000_000)
         .map(|_| rand::rng().random_range(1..=1000000))
         .collect();
     println!("rand ints generated in: {:?}", now.elapsed().unwrap());
     let now = SystemTime::now();
-    let s = arr.into_iter().sum::<i64>();
+    let s = arr_i64.clone().into_iter().sum::<i64>();
     println!("sum calculated in: {:?}", now.elapsed().unwrap());
-    println!("sum: {:?}", s)
+    println!("sum: {:?}", s);
+
+    println!("\n\n-----------------------------------");
+    println!("CHECKED maths performance test");
+    println!("-----------------------------------");
+    println!("i32");
+    let now = SystemTime::now();
+    let s = arr_i32.clone().into_iter().checked_sum();
+    let s = if let None = s {
+        println!("overflow occurred");
+        arr_i32.into_iter().map(|x| x as i64).checked_sum()
+    } else {
+        Some(s.unwrap() as i64)
+    };
+    println!("sum calculated in: {:?}", now.elapsed().unwrap());
+    println!("sum: {:?}", s);
+
+    println!("-----------------------------------");
+    println!("i64");
+    let now = SystemTime::now();
+    let s = arr_i64.into_iter().checked_sum();
+    println!("sum calculated in: {:?}", now.elapsed().unwrap());
+    println!("sum: {:?}", s);
 }
