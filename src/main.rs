08a88d479ab4080e8b34b1ccd7c8401ddd2cf653
Chris West (Faux) <git@goeswhere.com>
2025-09-21 11:37:25 +0100

    fix: eliminate clones

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,28 @@ use rayon::prelude::*;
 use std::time::SystemTime;
 
 fn main() {
-    println!("-----------------------------------");
-    println!("Unchecked maths performance test");
-    println!("-----------------------------------");
-    println!("i32");
     let now = SystemTime::now();
     let arr_i32: Vec<i32> = (1..=1_000_000_000)
         .into_par_iter()
         .map(|_| rand::rng().random_range(1..=1000000))
         .collect();
+    let arr_i64: Vec<i64> = arr_i32.iter().map(|&x| x as i64).collect();
     println!("rand ints generated in: {:?}", now.elapsed().unwrap());
+
+    println!("-----------------------------------");
+    println!("Unchecked maths performance test");
+    println!("-----------------------------------");
+
+    println!("i32");
     let now = SystemTime::now();
-    let s = arr_i32.clone().into_iter().sum::<i32>();
+    let s = arr_i32.iter().cloned().sum::<i32>();
     println!("sum calculated in: {:?}", now.elapsed().unwrap());
     println!("sum: {:?}", s);
 
     println!("-----------------------------------");
     println!("i64");
     let now = SystemTime::now();
-    let arr_i64: Vec<i64> = (1..=1_000_000_000)
-        .into_par_iter()
-        .map(|_| rand::rng().random_range(1..=1000000))
-        .collect();
-    println!("rand ints generated in: {:?}", now.elapsed().unwrap());
-    let now = SystemTime::now();
-    let s = arr_i64.clone().into_iter().sum::<i64>();
+    let s = arr_i64.iter().cloned().sum::<i64>();
     println!("sum calculated in: {:?}", now.elapsed().unwrap());
     println!("sum: {:?}", s);
 
@@ -37,7 +34,7 @@ fn main() {
     println!("-----------------------------------");
     println!("i32");
     let now = SystemTime::now();
-    let s = arr_i32.into_iter().map(|x| x as i64).checked_sum();
+    let s = arr_i32.iter().cloned().map(|x| x as i64).checked_sum();
     println!("sum calculated in: {:?}", now.elapsed().unwrap());
     println!("sum: {:?}", s);
     println!("sum (i32): {:?}", s.and_then(|s| i32::try_from(s).ok()));
@@ -45,7 +42,11 @@ fn main() {
     println!("-----------------------------------");
     println!("i64");
     let now = SystemTime::now();
-    let s = arr_i64.into_iter().checked_sum();
+    let s = arr_i64.iter().cloned().checked_sum();
     println!("sum calculated in: {:?}", now.elapsed().unwrap());
     println!("sum: {:?}", s);
+
+    // prevent (human) cheating
+    drop(arr_i32);
+    drop(arr_i64);
 }
